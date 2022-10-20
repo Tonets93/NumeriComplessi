5afc8fa32c318455fcd4bfdfdd7aae7242dd3417
Antonio <87380870+Tonets93@users.noreply.github.com>
2022-10-20 10:24:08 +0200

    Aggiornamento nomi variabili secondo convenzione

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Un numero complesso è costituito da una parte reale ed una immaginaria.
-complexNum1 = (3 + -2j)
-complexNum2 = (-1 + 5j)
+complex_num_1 = (3 + -2j)
+complex_num_2 = (-1 + 5j)
 
 # Restituiamo il tipo delle variabili dichiarate. In Python tutti i tipi sono oggetti.
-print(type(complexNum1))
-print(type(complexNum2))
+print(type(complex_num_1))
+print(type(complex_num_2))
 
 # Calcoliamo il prodotto fra due numeri complessi.
-complexProductResult = complexNum1 * complexNum2
-print("Il prodotto fra i due numeri complessi è: ", complexProductResult)
+complex_product_result = complex_num_1 * complex_num_2
+print("Il prodotto fra i due numeri complessi è: ", complex_product_result)
 
 # Accediamo alle proprietà 'real' e 'imag' dell'oggetto contenente il risultato.
-print("Parte reale: ", complexProductResult.real)
-print("Parte immaginaria: ", complexProductResult.imag)
+print("Parte reale: ", complex_product_result.real)
+print("Parte immaginaria: ", complex_product_result.imag)
 
